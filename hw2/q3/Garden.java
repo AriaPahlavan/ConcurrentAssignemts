90bd64660837cc34118f8ebbb017100f8c12db4a
Sharmistha Maity <smaity93@gmail.com>
2017-02-12 20:31:02 -0600

    garden and gardenTest complete

--- hw2/q3/Garden.java
+++ hw2/q3/Garden.java
@@ -1,73 +1,93 @@
+import java.security.Timestamp;
+import java.util.Date;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class Garden {
-    private final static  int DUG_HOLES_LIMIT = 4;
-    private final static  int UNFILLED_HOLES_LIMIT = 8;
+    private final static int DUG_HOLES_LIMIT = 4;
+    private final static int UNFILLED_HOLES_LIMIT = 8;
     private final ReentrantLock monitorLock = new ReentrantLock();
     private final Condition isEmptyHole = monitorLock.newCondition();
     private final Condition isSeedPlanted = monitorLock.newCondition();
     private final Condition isShovelTaken = monitorLock.newCondition();
     private final Condition reachedDugMax = monitorLock.newCondition();
     private final Condition reachedUnfilledMax = monitorLock.newCondition();
-    
+
     private int holesDug = 0;
     private int seedsPlanted = 0;
     private int unfilledHoles = 0;
-    
+    private boolean shovelTaken = false;
+
     public Garden() {
-        
+
     }
-    
+
     public void startDigging() throws InterruptedException {
         monitorLock.lock();
         try {
-            System.out.println("Newton is digging hole " + holesDug + 1);
-            isShovelTaken.await();
-            
-            if ( getUnseededHoles() == DUG_HOLES_LIMIT )
+
+            if (getUnseededHoles() == DUG_HOLES_LIMIT) {
+                System.out.println("Newton is waiting for Benjamin to finish seeding");
                 reachedDugMax.await();
-            
-            if ( unfilledHoles == UNFILLED_HOLES_LIMIT )
+            }
+
+            if (unfilledHoles == UNFILLED_HOLES_LIMIT) {
+                System.out.println("Newton is waiting for Mary to finish filling");
                 reachedUnfilledMax.await();
-            
+            }
+
+
+            while (shovelTaken) {
+                System.out.println("Newton is waiting for the shovel");
+                isShovelTaken.await();
+            }
+            System.out.println("Newton got the shovel");
+            shovelTaken = true;
         } finally {
+            System.out.println("Newton is digging hole " + (holesDug + 1) + " at " + (new Date().getTime() & 0xfff));
             monitorLock.unlock();
         }
     }
-    
+
     public void doneDigging() {
         monitorLock.lock();
         try {
-            holesDug++;
+            newHoleDug();
             System.out.println("Newton is done digging hole " + holesDug);
-
-            unfilledHoles++;
             isEmptyHole.signal();   //TODO order matters?
+            shovelTaken = false;
             isShovelTaken.signal(); //TODO maybe signalAll() is sufficient
         } finally {
             monitorLock.unlock();
         }
     }
-    
-    public void startSeeding() throws InterruptedException{
+
+    private void newHoleDug() {
+        holesDug++;
+        unfilledHoles++;
+    }
+
+    public void startSeeding() throws InterruptedException {
         monitorLock.lock();
         try {
-            System.out.println("Benjamin is seeding hole " + seedsPlanted+1);
-            isEmptyHole.await();
-            
+            if(getUnseededHoles()<1) {
+                System.out.println("Benjamin is waiting for an empty hole");
+                isEmptyHole.await();
+            }
+
         } finally {
+            System.out.println("Benjamin is seeding hole " + (seedsPlanted + 1)+ " at " + (new Date().getTime() & 0xfff));
             monitorLock.unlock();
         }
     }
-    
+
     public void doneSeeding() {
         monitorLock.lock();
         try {
             seedsPlanted++;
             System.out.println("Benjamin is done seeding hole " + seedsPlanted);
 
-            if(getUnseededHoles() < DUG_HOLES_LIMIT){
+            if (getUnseededHoles() < DUG_HOLES_LIMIT) {
                 reachedDugMax.signal();
             }
             isSeedPlanted.signal(); //TODO signalAll() ?
@@ -75,36 +95,46 @@ public class Garden {
             monitorLock.unlock();
         }
     }
-    
-    public void startFilling() throws InterruptedException{
+
+    public void startFilling() throws InterruptedException {
         monitorLock.lock();
         try {
-            System.out.println("Mary is filling hole " + unfilledHoles);
 
-            isShovelTaken.await();
-            isSeedPlanted.await();
-        
+            if(getUnfilledSeededHoles() < 1) {
+                System.out.println("Mary is waiting for a seed to be planted");
+                isSeedPlanted.await();
+            }
+
+            while (shovelTaken) {
+                System.out.println("Mary is waiting for the shovel");
+                isShovelTaken.await();
+            }
+            System.out.println("Mary got the shovel");
+            shovelTaken = true;
         } finally {
+            System.out.println("Mary is filling hole " + (totalHolesFilledByMary()+1)
+                                + " at " + (new Date().getTime() & 0xfff));
             monitorLock.unlock();
         }
     }
-    
+
     public void doneFilling() {
         monitorLock.lock();
         try {
             unfilledHoles--;
-            System.out.println("Mary is done filling hole " + unfilledHoles);
-            if(unfilledHoles < UNFILLED_HOLES_LIMIT){
+            System.out.println("Mary is done filling hole " + totalHolesFilledByMary());
+            if (unfilledHoles < UNFILLED_HOLES_LIMIT) {
                 reachedUnfilledMax.signal();
             }
+            shovelTaken = false;
             isShovelTaken.signal();
 
-        
+
         } finally {
             monitorLock.unlock();
         }
     }
-    
+
     /**
      * The following methods return the total number of holes dug, seeded or
      * filled by Newton, Benjamin or Mary at the time the methods' are
@@ -114,18 +144,22 @@ public class Garden {
 
         return holesDug;
     }
-    
+
     public int totalHolesSeededByBenjamin() {
 
         return seedsPlanted;
     }
-    
+
     public int totalHolesFilledByMary() {
 
         return holesDug - unfilledHoles;
     }
-    
+
     public int getUnseededHoles() {
         return holesDug - seedsPlanted;
+    }
+
+    public int getUnfilledSeededHoles() {
+        return unfilledHoles - getUnseededHoles();
     }
 }
--- hw2/q3/GardenTest.java
+++ hw2/q3/GardenTest.java
@@ -8,15 +8,10 @@ import static org.junit.Assert.*;
  * Created by Sharmistha on 2/12/2017.
  */
 public class GardenTest {
-    @Before
-    public void setUp() throws Exception {
-
-    }
-
     @Test
-    public void testGarden() throws Exception {
+    public void testGarden1() throws Exception {
         final Garden garden = new Garden();
-        final int plantsNeeded = 10;
+        final int plantsNeeded = 15;
 
         Thread newton = new Thread(new Runnable() {
             @Override
@@ -24,7 +19,7 @@ public class GardenTest {
                 for (int i = 0; i < plantsNeeded; i++) {
                     try {
                         garden.startDigging();
-                        sleep(1000);
+                        sleep(2);
                         garden.doneDigging();
                     } catch (InterruptedException e) {
                         e.printStackTrace();
@@ -40,7 +35,7 @@ public class GardenTest {
                 for (int i = 0; i < plantsNeeded; i++) {
                     try {
                         garden.startSeeding();
-                        sleep(1000);
+                        sleep(5);
                         garden.doneSeeding();
                     } catch (InterruptedException e) {
                         e.printStackTrace();
@@ -55,7 +50,136 @@ public class GardenTest {
                 for (int i = 0; i < plantsNeeded; i++) {
                     try {
                         garden.startFilling();
-                        sleep(1000);
+                        sleep(2);
+                        garden.doneFilling();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        newton.start();
+        benjamin.start();
+        mary.start();
+
+        newton.join();
+        benjamin.join();
+        mary.join();
+
+        System.out.println("Total holes dug: "+ garden.totalHolesDugByNewton());
+        System.out.println("Total holes seeded: "+ garden.totalHolesSeededByBenjamin());
+        System.out.println("Total holes filled: "+ garden.totalHolesFilledByMary());
+    }
+
+    @Test
+    public void testGarden2() throws Exception {
+
+        final Garden garden = new Garden();
+        final int plantsNeeded = 15;
+
+        Thread newton = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < plantsNeeded; i++) {
+                    try {
+                        garden.startDigging();
+                        sleep(8);
+                        garden.doneDigging();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            }
+        });
+
+        Thread benjamin = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < plantsNeeded; i++) {
+                    try {
+                        garden.startSeeding();
+                        sleep(1);
+                        garden.doneSeeding();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        Thread mary = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < plantsNeeded; i++) {
+                    try {
+                        garden.startFilling();
+                        sleep(6);
+                        garden.doneFilling();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        newton.start();
+        benjamin.start();
+        mary.start();
+
+        newton.join();
+        benjamin.join();
+        mary.join();
+
+        System.out.println("Total holes dug: "+ garden.totalHolesDugByNewton());
+        System.out.println("Total holes seeded: "+ garden.totalHolesSeededByBenjamin());
+        System.out.println("Total holes filled: "+ garden.totalHolesFilledByMary());
+    }
+
+    @Test
+    public void testGarden3() throws Exception {
+        final Garden garden = new Garden();
+        final int plantsNeeded = 15;
+
+        Thread newton = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < plantsNeeded; i++) {
+                    try {
+                        garden.startDigging();
+                        sleep(2);
+                        garden.doneDigging();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            }
+        });
+
+        Thread benjamin = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < plantsNeeded; i++) {
+                    try {
+                        garden.startSeeding();
+                        sleep(1);
+                        garden.doneSeeding();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        Thread mary = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < plantsNeeded; i++) {
+                    try {
+                        garden.startFilling();
+                        sleep(10);
                         garden.doneFilling();
                     } catch (InterruptedException e) {
                         e.printStackTrace();
